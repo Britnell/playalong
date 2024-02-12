95588dd5ca64b485101f9e486033df235560e83e
Britnell <tommy.britnell@gmail.com>
2024-02-12 08:29:34 +0100

    factor most likely only

--- src/comp/audio.tsx
+++ src/comp/audio.tsx
@@ -15,30 +15,33 @@ export default function Audio({ data }: { data: SongData }) {
 
 const Row = ({ data }: { data: Segment }) => {
   const height = data.duration * 100;
-  const one = data.pitches.findIndex((p) => p === 1);
+  // const one = data.pitches.findIndex((p) => p === 1);
+  const total = data.pitches.reduce((t, x) => t + x, 0);
 
   return (
     <div class="row" style={{ height: `${height}px` }}>
-      <div class="absolute left-0">
-        {/* {notes[one]} */}
-        <span class=" text-[0.8em]">
-          {Math.floor(data.confidence * 10) / 10}
-        </span>
-
-        {/* {data.start} */}
-      </div>
+      {/* <div class="absolute left-0">
+        {notes[one]}
+        <span class=" text-[0.8em]">{notes[one]}</span>
+        <span class=" text-[0.8em]">{dec(data.confidence)}</span>
+        {data.start}
+      </div> */}
       <div class="flex h-full">
         {data.pitches.map((pitch, p) => {
-          const show = pitch > 0.4;
-          const conf = show ? pitch * data.confidence : 0;
-          const color = highlight(conf);
+          // const conf = pitch > 0.4 ? data.confidence * pitch : 0;
+          const conf = (data.confidence * pitch) / total;
+          const min = 0.08;
           return (
             <div class={` key${p} `}>
-              <div style={{ "background-color": color }}>
-                <span class=" text-[0.8em]">
-                  {"   "}
-                  {Math.floor(pitch * 10) / 10}
-                </span>
+              <div
+                class=" rounded-sm"
+                style={{ "background-color": highlight(conf, min) }}
+              >
+                {/* <span class=" text-[0.8em]">
+                  {dec(conf)}
+                  {dec(pitch)}
+                </span> */}
+                {conf > min ? notes[p] : " "}
                 {/* {show ? notes[p] : " "} */}
               </div>
             </div>
@@ -49,9 +52,12 @@ const Row = ({ data }: { data: Segment }) => {
   );
 };
 
-const highlight = (perc: number) => {
+const highlight = (perc: number, min: number = 0.1, max: number = 0.6) => {
   let val = 0;
-  if (perc > 0.9) val = 1;
-  else val = linear(perc, 0, 0.9, 0, 1);
+  if (perc < min) val = 0;
+  else if (perc > max) val = 1;
+  else val = linear(perc, min, max, 0.2, 1);
   return `hsl(120deg 100% 50% / ${val})`;
 };
+
+// const dec = (x: number, digits: number = 10) => Math.floor(x * digits) / digits;
